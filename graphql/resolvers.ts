cdcdc430f7290f54f8cb84cf24e2445aa7f06100
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-12-02 17:34:56 +0000

    agregar comunicacion a microservicio de usuarios

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,13 +1,24 @@
 import { prisma } from '../config/prisma';
 
 const userResolvers = {
-  Query: {
-    users: async (parent, args, context) => {
-      return await prisma.user.findMany({
-        include: {
-          profile: true,
+  User: {
+    profile: async (parent, args, context) => {
+      return await prisma.profile.findUnique({
+        where: {
+          userId: parent.id,
         },
       });
+    },
+    author: async (parent, args, context) => {
+      const author =
+        await prisma.$queryRaw`select * from social."Author" as A where A."userId" = ${parent.id}`;
+      
+      return author[0];
+    },
+  },
+  Query: {
+    users: async (parent, args, context) => {
+      return await prisma.user.findMany();
     },
   },
 };
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import { ApolloServer } from 'apollo-server';
 import { userTypes } from './graphql/types';
 import { userResolvers } from './graphql/resolvers';
+import { buildSubgraphSchema } from '@apollo/subgraph';
 
 const server = new ApolloServer({
-  typeDefs: userTypes,
-  resolvers: userResolvers,
+  schema: buildSubgraphSchema([{ typeDefs: userTypes, resolvers: userResolvers }]),
 });
 
-server.listen({ port: 5000 }).then((url) => {
+server.listen({ port: 4001 }).then((url) => {
   console.log('Microservicio de usuarios corriendo');
 });
